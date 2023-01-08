5e2e8598b938afb7f2679a12fdff9febaab55c0e
rbustamanteAZ <reinal2btte@gmail.com>
2023-01-08 10:30:14 -0300

    Se agrega seguridad encriptando el password

---
+++ players/src/main/java/com/proyect/players/entity/User.java
@@ -0,0 +1,44 @@
+package com.proyect.players.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+
+@Entity
+public class User {
+
+    @jakarta.persistence.Id
+    @GeneratedValue
+    private Long Id;
+    private String username;
+    private String password;
+
+    public User(){
+
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+}
