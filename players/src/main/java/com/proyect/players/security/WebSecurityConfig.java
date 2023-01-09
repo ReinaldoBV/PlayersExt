412f19ca30be349cc86dec9ec80edd0607a48931
rbustamanteAZ <reinal2btte@gmail.com>
2023-01-09 20:05:28 -0300

    Update WebSecurityConfig.java

--- players/src/main/java/com/proyect/players/security/WebSecurityConfig.java
+++ players/src/main/java/com/proyect/players/security/WebSecurityConfig.java
@@ -1,26 +1,44 @@
 package com.proyect.players.security;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
-    
+
+    private final static 
+
 
     @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+            .setType(EmbeddedDatabaseType.H2)
+            .addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
+            .build();
+    }
+
+    @Bean
+    public UserDetailsManager users(DataSource dataSource) {
         UserDetails user = User.withDefaultPasswordEncoder()
-            .username("toronto")
-            .password("raptor")
+            .username("user")
+            .password("password")
             .roles("USER")
             .build();
-        return new InMemoryUserDetailsManager(user);
+        JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
+        users.createUser(user);
+        return users;
     }
 
 }
